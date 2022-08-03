b3496b3bdbf89a372a569df8c212048e857c6267
Felipe Reis <contacto.felipereis@gmail.com>
2022-08-03 16:57:25 +0100

    add detail field

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,3 +1,4 @@
 Nova.booting((Vue, store) => {
   Vue.component('DefaultField', require("./components/DefaultField").default)
+  Vue.component('PanelItem', require("./components/PanelItem").default)
 })
